b7fe0ac1c90cba8758fb69e07aebf3f629b9043a
Thomas Bueler <twbueler@gmail.com>
2020-06-24 15:55:04 -0800

    displays arrays of projections

--- anaylytic_numerical_statistical/anaylytic/proj1.m
+++ anaylytic_numerical_statistical/anaylytic/proj1.m
@@ -4,27 +4,22 @@ function proj = proj1 (xx)
 
     global eta eta_prime u u_prime beta
 
-    %s = xx + eta(xx);
     s = chebfun(@(x) x + eta(x), [0 20]);
     A = @(x) [0 1; beta^2*s(x) 0]; %beta^2*s
     B = [0 0; 1 0];
     
-    uiu = chebfun(@(x) 1, [0 20]);
+    %D = chebfun(@(x) ([1 0].*eta_prime(x))*eye(2) + ([1 0].*u_prime(x))*A(x), [0 20]);
     
-    disp("D")
-
+    D = @(x) eta_prime(x)*eye(2) + u_prime(x)*A(x);
     
-    
-    D = chebfun(@(x) ([1 0].*eta_prime(x))*eye(2) + ([1 0].*u_prime(x))*A(x), [0 20]);
     %D = chebfun(@(x) ([1 0].*eta_prime(x))*eye(2), [0 20]);
     %D = (1+eta_prime(2))*eye(2);
-    disp(D)
-    disp(D(:,:))
+    %disp(D(1))
+    %disp(inv(D(1)))
     
-    disp(inv(D))
-    phi0 = [u(xx) ; eta(xx)+(u(xx)^2)/2];
-    phi0_prime = [u_prime(xx); eta_prime(xx)+2*u(xx)*u_prime(xx)];
+    phi0 = @(x) [u(x) ; eta(x)+(u(x)^2)/2];
+    phi0_prime = @(x) [u_prime(x); eta_prime(x)+2*u(x)*u_prime(x)];
     
-    proj = phi0 + u(xx)*(u_prime(xx)*inv(D)*B*phi0 - B*phi0-A*inv(D)*phi0_prime);
+    proj = @(x) phi0(x) + u(x)*(u_prime(x)*inv(D(x)).*B.*phi0(x) - B.*phi0(x)-A.*inv(D(x)).*phi0_prime(x));
     
 end
